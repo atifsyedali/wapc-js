bb5408d44204ffb817abada44ac711f36fea0f6a
Atif Ali <atif.ali@hpe.com>
2023-09-25 14:44:58 -0400

    add initialize method for external import initialization

--- src/wapc-host.ts
+++ src/wapc-host.ts
@@ -5,6 +5,7 @@ import { HostCallNotImplementedError } from './errors';
 
 type HostCall = (binding: string, namespace: string, operation: string, payload: Uint8Array) => Uint8Array;
 type Writer = (message: string) => void;
+type InitializeCallback = (instance: WebAssembly.Instance) => Promise<void>;
 
 interface Invocation {
   operation: string;
@@ -50,8 +51,14 @@ export class WapcHost {
   textEncoder: TextEncoder;
   textDecoder: TextDecoder;
   wasiImports: WasiImports;
+  initializeCallback?: InitializeCallback;
 
-  constructor(hostCall?: HostCall, writer?: Writer, wasiImports?: WasiImports) {
+  constructor(
+    hostCall?: HostCall,
+    writer?: Writer,
+    wasiImports?: WasiImports,
+    initializeCallback?: InitializeCallback,
+  ) {
     this.state = new ModuleState(hostCall, writer);
     const generatedWasiImports = generateWASIImports(this);
     this.wasiImports = wasiImports ?? {
@@ -59,6 +66,7 @@ export class WapcHost {
       wasi_unstable: generatedWasiImports,
       wasi_snapshot_preview1: generatedWasiImports,
     };
+    this.initializeCallback = initializeCallback;
     this.textEncoder = new TextEncoder();
     this.textDecoder = new TextDecoder('utf-8');
     this.guestCall = () => undefined;
@@ -69,7 +77,7 @@ export class WapcHost {
     const result = await WebAssembly.instantiate(source, imports).catch(e => {
       throw new errors.InvalidWasm(e);
     });
-    this.initialize(result.instance);
+    await this.initialize(result.instance);
 
     return this;
   }
@@ -86,7 +94,7 @@ export class WapcHost {
       const result = await WebAssembly.instantiateStreaming(source, imports).catch(e => {
         throw new errors.StreamingFailure(e);
       });
-      this.initialize(result.instance);
+      await this.initialize(result.instance);
       return this;
     }
   }
@@ -98,8 +106,11 @@ export class WapcHost {
     };
   }
 
-  initialize(instance: WebAssembly.Instance): void {
+  async initialize(instance: WebAssembly.Instance): Promise<void> {
     this.instance = instance;
+    if (this.initializeCallback) {
+      await this.initializeCallback(instance);
+    }
     const start = this.instance.exports[START] as CallableFunction;
     if (start != null) {
       start([]);
@@ -146,7 +157,8 @@ export async function instantiateStreaming(
   hostCall?: HostCall,
   writer?: Writer,
   wasiImports?: WasiImports,
+  initializeCallback?: InitializeCallback,
 ): Promise<WapcHost> {
-  const host = new WapcHost(hostCall, writer, wasiImports);
+  const host = new WapcHost(hostCall, writer, wasiImports, initializeCallback);
   return host.instantiateStreaming(await source);
 }
